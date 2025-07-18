afcfef9f60be4ea87196ef34d88476934318caca
IvanBed <116827491+IvanBed@users.noreply.github.com>
2025-07-18 17:07:58 +0300

    Форматирование текста исходника main.c.

--- src/problem-1/main.c
+++ src/problem-1/main.c
@@ -12,7 +12,6 @@
 // Решение тасков 1, 2, 3, 5, 6
 
 static int create_stack(void** mystack, size_t* mystacksize) {
-
     *mystacksize = 2 * PTHREAD_STACK_MIN;   
     *mystack = aligned_alloc(PTHREAD_STACK_MIN, *mystacksize);
     
@@ -27,65 +26,59 @@ static int create_stack(void** mystack, size_t* mystacksize) {
     return 0;
 }
 
-
 static void* print_lines(void* data) {
-	int line_num = 1;
-	pthread_t id =  pthread_self(); 
+    int line_num = 1;
+    pthread_t id =  pthread_self(); 
     struct sched_param thred_param;
     int policy = 0;
-	int s = pthread_getschedparam (id, &policy, &thred_param);
+    int s = pthread_getschedparam(id, &policy, &thred_param);
         if (s != 0)
             printf("Problems");
     s = pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
         if (s != 0)
             handle_error_en(s, "pthread_setcancelstate");
 
-
-	for (size_t i = 0; i < 10; i++)
-		printf("%lu thread line # %d, priority is %d\n", id, line_num++, thred_param.sched_priority);
-	
-	printf("line_num pointer points ----> %p\n", &line_num);
-	
-	return 0;
+    for (size_t i = 0; i < 10; i++)
+        printf("%lu thread line # %d, priority is %d\n", id, line_num++, thred_param.sched_priority);
+    
+    printf("line_num pointer points ----> %p\n", &line_num);
+    
+    return 0;
 }
-
 
 int main(int argc, char** argv) {
     setvbuf(stdout, NULL, _IONBF, 0);
-	pthread_t thread1;
+    pthread_t thread1;
     pthread_attr_t attr; 
-	void *res;
-	
-   
+    void *res;
+    
     void* mystack;                                                                            
     size_t stacksize;
  
-	int err = create_stack(&mystack, &stacksize);
-	if (err != 0) {
-		pthread_attr_destroy(&attr);
-		exit(2);
-	}
-		
-	pthread_attr_init(&attr);
-	
-	pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
-	
-	struct sched_param param;
-	pthread_attr_getschedparam(&attr, &param);
-	param.sched_priority = 3;
-	pthread_attr_setschedparam(&attr, &param);	
-	
-	
-	
-    pthread_create(&thread1, &attr, print_lines, NULL);	
-	
-	int line_num = 1;
-	for (size_t i = 0; i < 10; i++)
-		printf("Main thread line # %d\n", line_num++);
-	
-	printf("Main thread line_num pointer points ----> %p\n", &line_num);
-	
-	int s = pthread_join(thread1, NULL);
+    int err = create_stack(&mystack, &stacksize);
+    if (err != 0) {
+        pthread_attr_destroy(&attr);
+        exit(2);
+    }
+        
+    pthread_attr_init(&attr);
+    
+    pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
+    
+    struct sched_param param;
+    pthread_attr_getschedparam(&attr, &param);
+    param.sched_priority = 3;
+    pthread_attr_setschedparam(&attr, &param);    
+    
+    pthread_create(&thread1, &attr, print_lines, NULL);    
+    
+    int line_num = 1;
+    for (size_t i = 0; i < 10; i++)
+        printf("Main thread line # %d\n", line_num++);
+    
+    printf("Main thread line_num pointer points ----> %p\n", &line_num);
+    
+    int s = pthread_join(thread1, NULL);
     if (s != 0)
         handle_error_en(s, "pthread_join");
 
@@ -95,9 +88,9 @@ int main(int argc, char** argv) {
         printf("%s(): thread wasn't canceled (shouldn't happen!)\n",
             __func__);
           
-	
-	free(mystack);  // Освобождаем стек
+    
+    free(mystack);  // Освобождаем стек
     pthread_attr_destroy(&attr);  // Уничтожаем атрибуты  
-	exit(EXIT_SUCCESS);
-	return 0;
+    exit(EXIT_SUCCESS);
+    return 0;
 }
