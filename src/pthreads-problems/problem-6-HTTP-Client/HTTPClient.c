e13bdc7aab83c633e55a6fdaa9e3f2e96ac57da2
IvanBed <116827491+IvanBed@users.noreply.github.com>
2025-08-01 12:15:49 +0300

    Добавление тестового соединения с хостом по доменному имени.

--- src/pthreads-problems/problem-6-HTTP-Client/HTTPClient.c
+++ src/pthreads-problems/problem-6-HTTP-Client/HTTPClient.c
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <errno.h> 
 
 #define BUF_SIZE 512
 
@@ -121,7 +122,19 @@ static void print_list(struct Node* list) {
     }
 }
 
-static int connect_with_domainname(char* domain_name, struct addrinfo** socket_id) {
+int free_list(struct Node* list) {
+    struct Node* cur = list;
+    while (cur) {
+        struct Node* temp = cur;
+        cur = cur->next;
+        free(temp->str_val);
+        free(temp);
+    }
+}
+
+
+// Разбить на две функциии, одна получает ip по доменному имени, другая устанавливает соединение.
+static int create_socket_to_domain (char* domain_name, int* socket_id) {
 	struct addrinfo hints;
     struct addrinfo *result, *rp;
     int sfd, s, j;
@@ -137,16 +150,11 @@ static int connect_with_domainname(char* domain_name, struct addrinfo** socket_i
     hints.ai_flags = 0;
     hints.ai_protocol = 0;          /* Any protocol */
 
-    s = getaddrinfo(domain_name, 80, &hints, &result);
+    s = getaddrinfo(domain_name, "80", &hints, &result);
     if (s != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
         exit(EXIT_FAILURE);
     }
-
-    /* getaddrinfo() returns a list of address structures.
-       Try each address until we successfully connect(2).
-       If socket(2) (or connect(2)) fails, we (close the socket
-       and) try the next address. */
 
     for (rp = result; rp != NULL; rp = rp->ai_next) {
         sfd = socket(rp->ai_family, rp->ai_socktype,
@@ -155,39 +163,68 @@ static int connect_with_domainname(char* domain_name, struct addrinfo** socket_i
             continue;
 
         if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1) {
-		    puts("success");
+		    puts("Success");
 			break; 
 	    }
         close(sfd);
     }
 	
-    if (rp == NULL) {               /* No address succeeded */
+    if (rp == NULL) {              
         fprintf(stderr, "Could not connect\n");
         return 1;
     }
-	*socket_id = rp;
-    freeaddrinfo(result);           /* No longer needed */
+
+	*socket_id = sfd;
+    freeaddrinfo(result);           
     return 0;
 }
 
 
 int main(int argc, char** argv) {
+
+    if (argc < 2) {
+        printf("");
+        return 0;
+    }
 	
-	/*if (argc < 2) {
-		printf("");
-		return 0;
-	}
-	char* URL = argv[1];
-	*/
-	struct Node* list = NULL;
-	char* URL = "https://www.habr.com/";
-	int parse_res = parseURL(&list, URL);
-	print_list(list);
-	struct addrinfo* socket_id;
-	struct Node* domain_name_node = get_node(list, 2);
-	if (domain_name_node == NULL)
-	    return 1;
-	char* domain_name = domain_name_node->str_val;
-	puts(domain_name);
-	connect_with_domainname(domain_name, &socket_id);
+    char* URL = argv[1];
+    struct Node* list = NULL;
+    int sockfd = 0;
+
+    int parse_res = parseURL(&list, URL);
+    struct Node* domain_name_node = get_node(list, 2);
+
+    if (domain_name_node == NULL)
+        return 1;
+	
+    char* domain_name = domain_name_node->str_val;
+    puts(domain_name);
+    create_socket_to_domain(domain_name, &sockfd);
+
+    char *msg = "GET /ru/articles/931410/ HTTP/1.1 \
+                        Host: www.habr.com \
+                        User-Agent: Mozilla/5.0 \
+                        Accept: application/json";
+    int len, bytes_sent;
+
+    len = strlen(msg);
+    bytes_sent = send(sockfd, msg, len, 0);
+    char buff[256];
+    len = 256;
+    size_t count;
+	
+    while ((count = recv(sockfd, (void*) buff, len, 0)) != 0) {
+        puts(buff);
+        if (count == -1) {
+            switch (errno) {
+                case EAGAIN:
+                usleep(20000);
+                break;
+            default:
+                perror("Failed to read from socket");
+                close(sockfd);
+                break;
+            }
+        }
+    }
 }
