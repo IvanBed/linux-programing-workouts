74c1085f7fabf55d2a3cdb6fe27181bc1ed9a640
IvanBed <116827491+IvanBed@users.noreply.github.com>
2025-08-26 11:49:02 +0300

    Реализация работы потоков (поток-читатель и поток-писатель)

--- src/pthreads-problems/problem-7/main.cpp
+++ src/pthreads-problems/problem-7/main.cpp
@@ -1,89 +1,75 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
 #include "store_ll.h"
 
-
-static phthread_mutex_t m;
+static int exit_signal = 0;
+static pthread_mutex_t m;
 static pthread_cond_t cond;
-static int cond_predicate;
 
 void* get_data(void *data) {
 
-    struct Store *store = (sturct Store*) data;
+    struct Store *store = (struct Store*) data;
     
-    while (1) {
-        phtread_mutex_lock(&m, NULL);
-        while (cond_predicte != 1)
-            pthread_condition_wait(&cond, &m);
-            cond_predicate == 0;
-        if (store->size == 0) {
-             pthread_condition_signal(&cond, &m);
-             pthread_mutex_unlock(&m, NULL);
-             cond_predicate = 1;
-             sleep(10);
-        } else {
-             int res = 0;
-             get_value(&(*store->head), &res);
-             store->size--;
-             std::cout << "the value: " << res << " has gotten. \n";
-             pthread_condition_signal(&cond, &m);
-             cond_predicate = 1;
-             pthread_mutex_unlock(&m, NULL);
-        }
+    while (!exit_signal) {
+        pthread_mutex_lock(&m);
+        while (store->size < 0)
+            pthread_cond_wait(&cond, &m);
+            
 
-    }
+        int res = 0;
+        get_val2(&store, &res);
+         
+        printf("the value: %d has gotten. \n", res);
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&m);
+		sleep(2);
+	}
 
+	
 }
 
 
-void* set_data() {
+void* set_data(void *data) {
 
- struct Store *store = (sturct Store*) data;
+    struct Store *store = (struct Store*) data;
     
-    while (1) {
-        phtread_mutex_lock(&m, NULL);
-        while (cond_predicte != 1)
-            pthread_condition_wait(&cond, &m);
-       
-        if (store->size >= store->capacity) {
-            pthread_condition_signal(&cond, &m);
-            pthread_mutex_unlock(&m, NULL);
-            cond_predicate = 1;
-            sleep(10);
-        } else {
-            add_val(&(*store->head), 2);
-             store->size++;
-             pthread_condition_signal(&cond, &m);
-             cond_predicate = 1;
-             pthread_mutex_unlock(&m, NULL);
-
-       }
- 
+    while (!exit_signal) {
+        pthread_mutex_lock(&m);
+        while (store->size >= store->capacity)
+			pthread_cond_wait(&cond, &m);
+  
+        add_val(&store, 2);
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&m);
+		sleep(2);
    }
 
 }
 
 
 int main(int argc, char** argv) {
-    pthread_mutex_init(&m);
-    pthread_condition_init(&cond);
+    pthread_mutex_init(&m, NULL);
+    pthread_cond_init(&cond, NULL);
     pthread_t thread_setter, thread_getter;
+	struct Store *store_inst = NULL;
+	store_init(&store_inst, 10);
 
-    if (pthread_create(thread_setter, NULL, get_data, store) != 0) {
-        std::cout << "Could not start setter thread\n";
+    if (pthread_create(&thread_setter, NULL, get_data, store_inst) != 0) {
+        puts("Could not start setter thread");
         return 1;
     }
 
-    if (pthread_create(thread_getter, NULL, set_data, store) != 0) { 
-        std::cout << "Could not start setter thread\n";
+    if (pthread_create(&thread_getter, NULL, set_data, store_inst) != 0) { 
+        puts("Could not start setter thread");
         return 1;
     }
-
+ 
     pthread_join(thread_setter, NULL);
     pthread_join(thread_getter, NULL);
 
     pthread_mutex_destroy(&m);
-    pthread_condition_destroy(&cond);
+    pthread_cond_destroy(&cond);
 
     return 0;
 }
