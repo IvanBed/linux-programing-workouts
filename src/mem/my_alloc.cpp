eb9ea9d9da5f8f6964c1cc486ed5c4548a57cb8c
IvanBed <116827491+IvanBed@users.noreply.github.com>
2025-10-17 16:45:18 +0300

    Update my_alloc.cpp

--- src/mem/my_alloc.cpp
+++ src/mem/my_alloc.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <stdlib.h>
 
@@ -12,8 +11,20 @@ using namespace std;
     //       должен распределять, все возвращаемые указатели должны быть
     //       либо равны NULL, либо быть из этого участка памяти
     // size - размер участка памяти, на который указывает buf
+static void* head;
+static void *max_ptr_val;
+static std::size_t list_size;
+
+static void set_size(void *, std::size_t);
+static void set_next(void *, void *);
+static void set_prev(void *, void *);
+
 void mysetup(void* buf, std::size_t size)
-{}
+{
+    head = buf;
+    list_size = size;
+    set_size(buf, size);
+}
 
 // Функция аллокации
 void* myalloc(std::size_t size)
@@ -22,8 +33,55 @@ void* myalloc(std::size_t size)
 // Функция освобождения
 void myfree(void* p)
 {}
-//  выделяем 32 байта
-int main() {
+
+
+static void set_size(void * cur_node, std::size_t size)
+{
+    void* size_ptr = cur_node;
+    *(std::size_t*)size_ptr = size;
+}
+
+
+static std::size_t get_size(void * cur_node)
+{
+    void* size_ptr = cur_node;
+    return *(std::size_t*)size_ptr;
+}
+
+
+static void set_next(void * cur_node, void * next_node)
+{
+    void* next = cur_node + 1;
+    next = next_node;
+}
+
+
+static void * get_next(void * cur_node)
+{
+    return cur_node + 1;
+}
+
+
+static void set_prev(void * cur_node, void * prev_node)
+{
+    void* prev = cur_node + 2;
+    prev = prev_node;
+}
+
+
+static void * get_prev(void * cur_node)
+{
+    return cur_node + 2;
+}
+
+
+static void * get_data(void * cur_node)
+{
+    return cur_node + 3;
+}
+
+
+/*int main() {
     void* blc = malloc(1032);
     std::size_t size_info = 1024;
 
@@ -41,3 +99,4 @@ int main() {
     std::cout << *((size_t*)size_ptr) << std::endl;
     std::cout << sizeof(void*) << std::endl;
 }
+*/
