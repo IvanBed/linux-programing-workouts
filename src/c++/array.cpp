b28fb3804e4bcab8072020c7c943a1c74e35c5fc
IvanBed <116827491+IvanBed@users.noreply.github.com>
2025-10-30 22:29:22 +0300

    Create array.cpp

---
+++ src/c++/array.cpp
@@ -0,0 +1,75 @@
+#include <cstddef>
+
+template <typename T>
+class Array
+{
+private:
+    T * data;
+    size_t size_;
+public: 
+    explicit Array(size_t size, const T& value = T()) 
+    {
+        size_ = size;
+        
+        data = (T*)new char[sizeof(T) * size];
+        for (size_t i = 0; i < size; i++)
+        {
+           data[i] = value; 
+        }      
+    }
+
+    Array()
+    {
+        data = nullptr;
+        size_ = 0;
+    }
+           
+    Array(const Array & arr)
+    {
+        T *new_data = (T*)new char[sizeof(T) * arr.size()];
+        size_ = arr.size();
+        
+        for(size_t i = 0; i < size_; i++)
+            new_data[i] = arr[i];  
+        
+        data = new_data;
+    }
+    
+    ~Array()
+    {
+        //for(size_t i = 0; i < size_; i++)
+        //{
+            delete data;  
+        //}
+    }
+    
+    size_t size() const
+    {
+        return size_;
+    }
+    
+    // Уточнить про перегрузку оператора сравнения
+    Array & operator=(const Array & arr)
+    {
+        if(&arr != this)
+        {
+            T *new_data = (T*)new char[sizeof(T) * arr.size()];
+            size_ = arr.size();
+            for(size_t i = 0; i < size_; i++)
+                new_data[i] = arr[i];
+            delete data;
+            data = new_data;
+        }
+        return *this;
+    }
+ 
+    T & operator[](size_t i)
+    {
+        return data[i];
+    }
+    
+    T operator[](size_t i) const
+    {
+        return data[i];
+    }   
+};
