c94635734ece169d2b5860284f59d75d23a32ecf
IvanBed <116827491+IvanBed@users.noreply.github.com>
2025-12-08 12:45:54 +0300

    Реализация функции mapper

--- src/c++/map-reduce/shortest-path-problem/mapper.cpp
+++ src/c++/map-reduce/shortest-path-problem/mapper.cpp
@@ -4,38 +4,28 @@
 struct vertex_info
 {
     size_t vertex_id;
-    size_t dist;
-    std::vector<size_t> adj_vertex;
+    int64_t dist;
+    std::vector<std::string> adj_vertex;
 };
 
-std::vector<size_t> split_line(std::string const & line, char separator)
+std::vector<std::string> split_line(std::string const & line, char separator)
 {
-    std::vector<size_t> res;
-	try
-	{
-        std::string val = "";
-        for (size_t i = 0; i < line.size(); i++)
+    std::vector<std::string> res;
+    std::string val = "";
+    for (size_t i = 0; i < line.size(); i++)
+    {
+        if(line[i] != separator)
         {
-            if(line[i] != separator)
-            {
-                val += line[i];
-            }
-            else 
-            {
-                res.push_back((size_t)std::stoi(val));
-                val = "";
-			}
+            val += line[i];
         }
-        res.push_back((size_t)std::stoi(val));
-    }  
-    catch (std::invalid_argument const & e) 
-	{
-        std::cout << e.what() << "\n";
+        else 
+        {
+            if (val != "") res.push_back(val);
+            val = "";
+        }
     }
-    catch (std::out_of_range const & e) 
-	{
-        std::cout << e.what() << "\n";
-    }
+    if (val != "") res.push_back(val);
+
     return res;
 }
 
@@ -45,7 +35,7 @@ size_t get_vertex_id(std::string const & line)
     try
     {
         size_t tab_pos = line.find('\t');
-        res = (size_t)stoi(line.substr(0, tab_pos));
+        res = (size_t)std::stoi(line.substr(0, tab_pos));
     }
     catch (std::invalid_argument const & e) 
     {
@@ -60,12 +50,16 @@ size_t get_vertex_id(std::string const & line)
 
 size_t get_distance_id(std::string const & line)
 {
-    size_t res;
+    int64_t res;
     try
     {
         size_t start = line.find('\t') + 1;
         size_t end = line.find('\t', start);
-        res = (size_t)stoi(line.substr(start, end - start));
+        std::string dist = line.substr(start, end - start);
+        if (dist == "INF")
+            res = -1;
+        else 
+            res = (int64_t)std::stoi(dist);
     }
     catch (std::invalid_argument const & e) 
     {
@@ -78,9 +72,9 @@ size_t get_distance_id(std::string const & line)
     return res;
 }
 
-std::vector<size_t> get_adj_list(std::string const & line)
+std::vector<std::string> get_adj_list(std::string const & line)
 {
-    std::vector<size_t> res;
+    std::vector<std::string> res;
     try
     {
         size_t start = line.find('{') + 1;
@@ -106,17 +100,35 @@ void parse_line(std::string const & line, struct vertex_info & cur)
     cur.adj_vertex = get_adj_list(line);
 }
 
+inline void print_vector(std::vector<std::string> const & vect, int64_t dist)
+{
+    std::string dist_str = "";
+    if (dist == -1)
+        dist_str = "INF";
+    else 
+        dist_str = std::to_string(++dist);
+    
+    for(auto const & el : vect)
+        std::cout << el << "\t" << dist_str << "\t" << "{}\n";
+    
+}
 
-int main() {
-    
+void mapper()
+{
     std::string line = "";
-    
+    struct vertex_info cur_info;
     while (getline(std::cin, line))
     {
-        
-        
-        
+        std::cout << line << "\n";
+        parse_line(line, cur_info);
+        if (!cur_info.adj_vertex.empty())
+            print_vector(cur_info.adj_vertex, cur_info.dist);
+        line = "";
     }
-    
+}
+
+int main() 
+{
+    mapper();	
     return 0;
 }
