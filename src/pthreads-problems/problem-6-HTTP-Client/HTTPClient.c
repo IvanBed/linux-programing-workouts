26c1549ea46973fb4d3f05f05687ceca22d5b5cf
Ivan Bedaev <ivanbedaev1@gmail.com>
2026-02-24 21:44:07 +0300

    Начал доделывать простой HTTP клиент

--- src/pthreads-problems/problem-6-HTTP-Client/HTTPClient.c
+++ src/pthreads-problems/problem-6-HTTP-Client/HTTPClient.c
@@ -9,15 +9,27 @@
 
 #define BUF_SIZE 512
 
-struct Node {
-	char* str_val;
-	struct Node* next;
-	size_t index;
+#define true  1
+#define false 0
+
+
+enum request_type 
+{ 
+    GET, POST
 };
 
-static int cpy_str(char** new_str, char* old_str) {
+struct Node 
+{
+    char* str_val;
+    struct Node* next;
+    size_t index;
+};
 
-    if (old_str == NULL) {
+static int cpy_str(char **new_str, char *old_str) 
+{
+
+    if (old_str == NULL) 
+    {
         *new_str = NULL;
         return 0;
     }
@@ -25,7 +37,8 @@ static int cpy_str(char** new_str, char* old_str) {
     size_t str_len = strlen(old_str) + 1;
     *new_str = (char*)malloc(str_len);
    
-    if (*new_str == NULL) {
+    if (*new_str == NULL) 
+    {
         perror("Memory for the string can not be allocated.");
         return 1;
     }
@@ -33,195 +46,342 @@ static int cpy_str(char** new_str, char* old_str) {
     memcpy(*new_str, old_str, str_len);
    
     return 0;
-	
+    
 } 
 
-static int make_node(struct Node** node, char* val) {
+static int make_node(struct Node **node, char *val) 
+{
 
-	*node = (struct Node*) malloc(sizeof(struct Node*));
-	 
-	if (*node == NULL) {
-		
-		return 1;
-	}
-	char* new_val;
-		
-	if (cpy_str(&new_val, val) != 0) {
-		return 1;
-	}
+    *node = (struct Node*) malloc(sizeof(struct Node*));
+     
+    if (*node == NULL) 
+    {
+        
+        return 1;
+    }
+    char *new_val = NULL;
+        
+    if (cpy_str(&new_val, val) != 0) 
+    {
+        return 1;
+    }
 
-	(*node)->str_val = new_val;
-	(*node)->next = NULL;
-	
+    (*node)->str_val = new_val;
+    (*node)->next = NULL;
+    
 }
 
-static int add(struct Node** head, char* val) {
+static int add(struct Node **head, char *val)
+{
 
-	struct Node* new_node;
-	int res = make_node(&new_node, val);
-	if (!res) {
-		
-		return 1;
-	}
-	if (*head == NULL) {
-		*head = new_node;
-		return 0;
-	}
+    struct Node *new_node;
+    int res = make_node(&new_node, val);
+    if (!res) {
+        
+        return 1;
+    }
+    if (*head == NULL) 
+    {
+        *head = new_node;
+        return 0;
+    }
 
-	struct Node* cur = *head;
+    struct Node* cur = *head;
 
-	while (cur->next != NULL)
-		cur = cur->next;
+    while (cur->next != NULL)
+        cur = cur->next;
 
-	cur->next = new_node;
+    cur->next = new_node;
   
-	new_node->index = cur->index + 1;
-	
+    new_node->index = cur->index + 1;
+    
 }
 
-static int parseURL(struct Node** list, char* URLstr) {
-
-	char str_buff[2048];
-	size_t buff_ptr = 0;
-	size_t url_len = strlen(URLstr);
-	memset(str_buff, 0, 2048 * sizeof(char));
-	printf("%d\n", url_len);
-	for (size_t i = 0; i < url_len && buff_ptr < 2047; i++) {
-	 
-		if (URLstr[i] == '/' || i + 1 == url_len) {
-                        if (i + 1 == url_len)
-                            str_buff[buff_ptr++] = URLstr[i];
-			str_buff[++buff_ptr] = '\0';
-			int res_of_add = add(list, str_buff);
-			memset(str_buff, 0, buff_ptr * sizeof(char));
-			buff_ptr = 0;
-		} else {
-		    str_buff[buff_ptr++] = URLstr[i];
-		}
-	
-	}
-
-}
-
-static struct Node* get_node(struct Node* list, size_t index) {
-	struct Node* cur = list;
-    while (cur) {
+static struct Node* get_node(struct Node *list, size_t index) 
+{
+    struct Node* cur = list;
+    while (cur) 
+    {
         if (cur->index == index)
-			return cur;
+            return cur;
         cur = cur->next;
     }
-	return NULL;
+    return NULL;
 }
 
-static void print_list(struct Node* list) {
+static void print_list(struct Node *list) 
+{
      
     struct Node* cur = list;
-    while (cur) {
-        printf("val %s index: %d\n", cur->str_val, cur->index);
+    while (cur) 
+    {
+        printf("val %s index: %ld\n", cur->str_val, cur->index);
         cur = cur->next;
     }
 }
 
-int free_list(struct Node* list) {
+int free_list(struct Node *list) 
+{
     struct Node* cur = list;
-    while (cur) {
-        struct Node* temp = cur;
+    while (cur) 
+    {
+        struct Node *temp = cur;
         cur = cur->next;
         free(temp->str_val);
         free(temp);
     }
 }
 
-static int get_ip_from_domain(char* domain_name, const char* port, struct addrinfo** result) {
-	struct addrinfo hints;
-	int err;
-	memset(&hints, 0, sizeof(struct addrinfo));
-	
+uint8_t empty(char const * str)
+{
+    if (str == NULL || str == "")
+        return true;
+    else
+        return false;
+}
+
+static int parseURL(struct Node **list, char *URLstr) 
+{
+    char str_buff[2048];
+    size_t buff_ptr = 0;
+    size_t url_len = strlen(URLstr);
+    memset(str_buff, 0, 2048 * sizeof(char));
+    for (size_t i = 0; i < url_len && buff_ptr < 2047; i++) 
+    {
+     
+        if (URLstr[i] == '/' || i + 1 == url_len) 
+        {
+            if (i + 1 == url_len)
+                str_buff[buff_ptr++] = URLstr[i];
+
+            str_buff[++buff_ptr] = '\0';
+            int res_of_add = add(list, str_buff);
+            memset(str_buff, 0, buff_ptr * sizeof(char));
+            buff_ptr = 0;
+        } 
+        else 
+        {
+            str_buff[buff_ptr++] = URLstr[i];
+        }
+    }
+    return 0;
+}
+
+static int make_domain_name(struct Node* list, char ** domain_name)
+{
+    struct Node* domain_name_node = get_node(list, 2);
+    if (domain_name_node == NULL)
+        return 1;
+    
+    * domain_name = domain_name_node->str_val;
+    return 0;
+}
+
+static int make_resourсe_url(struct Node *list, char **resourсe_url_res, size_t start_pos, size_t end_pos)
+{
+    size_t total_len = 0;
+    size_t offset    = 0;
+
+    struct Node *start = get_node(list, start_pos);
+    if (!start)
+    {
+        return 1;
+    }
+
+    while (start)
+    {
+        total_len += strlen(start->str_val);
+        start = start->next;
+    }
+    
+    char *buff = (char*) malloc(total_len + 1);
+    
+    start = get_node(list, 3);
+    while (start)
+    {
+        strcpy(buff + offset, start->str_val);
+        offset += strlen(start->str_val);
+        strcpy(buff + offset, "/");
+        offset++;
+        start = start->next;
+    }
+    buff[total_len] = '\0';
+
+    *resourсe_url_res = buff;
+
+    return 0;
+}
+
+static int make_http_request_msg(char const *domain_name, char const *resourсe_url, enum request_type type, char **http_request_msg)
+{
+    if (empty(domain_name) || empty(resourсe_url))
+    {
+        return 1;
+    }
+    
+    char const *protocol_str  = " HTTP/1.1\r\n";
+    char const *conn_type_str = " Connection: close\r\n\r\n";
+    char const *host_str      = "Host: ";
+    char *method_str          = "";
+    
+    switch (type)
+    {
+    case GET:
+        method_str = "GET ";
+        break;
+        
+    case POST:
+        method_str = "POST ";
+        break;
+
+    default:
+        break;
+    }
+
+    size_t domain_len        = strlen(domain_name);
+    size_t resourсe_url_len  = strlen(resourсe_url);
+    size_t protocol_len      = strlen(protocol_str);
+    size_t conn_type_len     = strlen(conn_type_str);
+    size_t host_str_len      = strlen(host_str);
+    size_t method_len        = strlen(method_str);
+    size_t offset            = 0;
+    size_t total_len         =  domain_len + resourсe_url_len + protocol_len + conn_type_len + host_str_len 
+    + method_len;
+
+    char *buff = (char*) malloc(total_len + 1);
+    if (!buff)
+    {
+        return 2;
+    }
+
+    strcpy(buff + offset, method_str);
+    offset += method_len;
+    strcpy(buff + offset, resourсe_url);
+    offset += resourсe_url_len;
+    strcpy(buff + offset, protocol_str);
+    offset += protocol_len;
+    strcpy(buff + offset, host_str);
+    offset += host_str_len;
+    strcpy(buff + offset, domain_name);
+    offset += domain_len;   
+    strcpy(buff + offset, conn_type_str);
+    buff[total_len] = '\0';
+    
+    *http_request_msg = buff;
+
+    return 0;
+}
+
+static int get_ip_from_domain(char *domain_name, const char *port, struct addrinfo **result) 
+{
+    struct addrinfo hints;
+    int err;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    
     hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
     hints.ai_socktype = SOCK_STREAM ; /* TCP socket */
     hints.ai_flags = 0;
     hints.ai_protocol = 0;          /* Any protocol */
 
     err = getaddrinfo(domain_name, port, &hints, result);
-	if (err != 0) {
+    if (err != 0) 
+    {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(err));
         return 1;
     }
-	
-	//freeaddrinfo(&hints);
-	return 0;
+    
+    //freeaddrinfo(&hints);
+    return 0;
 }
 
-static int try_connect(struct addrinfo* addr_info, int* sockfd) {
-	struct addrinfo* rp;
-	for (rp = addr_info; rp != NULL; rp = rp->ai_next) {
+static int try_connect(struct addrinfo *addr_info, int *sockfd) 
+{
+    struct addrinfo *rp;
+    for (rp = addr_info; rp != NULL; rp = rp->ai_next)
+    {
         *sockfd = socket(rp->ai_family, rp->ai_socktype,
             rp->ai_protocol);
         if (*sockfd == -1)
             continue;
 
-        if (connect(*sockfd, rp->ai_addr, rp->ai_addrlen) != -1) {
-		    puts("Success");
-			break; 
-	    }
-		close(*sockfd);
+        if (connect(*sockfd, rp->ai_addr, rp->ai_addrlen) != -1) 
+        {
+            puts("Success");
+            break; 
+        }
+        close(*sockfd);
     }
-	
-    if (rp == NULL) {              
+    
+    if (rp == NULL) 
+    {              
         fprintf(stderr, "Could not connect\n");
         return 1;
     }
 
-	return 0;
+    return 0;
 }
 
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 
     if (argc < 2) {
         printf("");
         return 0;
     }
-	
+    
     char* URL = argv[1];
     struct Node* list = NULL;
     int sockfd = 0;
-	struct addrinfo* result;
-	
-	
+    struct addrinfo* result;
+    
     int err = parseURL(&list, URL);
     if (err != 0) {
-		fprintf(stderr, "Problem with parsing URL.\n");
-		return 1;
-	}
-	
-	struct Node* domain_name_node = get_node(list, 2);
-
-    if (domain_name_node == NULL)
+        fprintf(stderr, "Problem with parsing URL.\n");
         return 1;
-	
-    char* domain_name = domain_name_node->str_val;
-    puts(domain_name);
+    }
     
-	err = get_ip_from_domain(domain_name, "80", &result);
-	if (err != 0) {
-		fprintf(stderr, "Can not resolve the domain name.\n");
-		return 1;
-	}
+    if (!check_protocol(list))
+    {
+        return 1;
+    }
 
-	err = try_connect(result, &sockfd);
-	if (err != 0) {
-		fprintf(stderr, "Can not connect to the host.\n");
-		return 1;
-	}
-	
-	freeaddrinfo(result);
-	
-    const char* msg = 
-        "GET / HTTP/1.1\r\n"
-        "Host: www.ccfit.nsu.ru\r\n"
-        "Connection: close\r\n"
-        "\r\n";
+    char *domain_name = NULL;
+    err = make_domain_name(list, &domain_name);
+    if (err != 0) {
+        fprintf(stderr, "Can not make resoutce url string.\n");
+        return 1;
+    }   
+    
+    char* resourсe_url = NULL;
+    err = make_resourсe_url(list, &resourсe_url, 3, 10);
+    if (err != 0) {
+        fprintf(stderr, "Can not make resoutce url string.\n");
+        return 1;
+    }
+    
+    err = get_ip_from_domain(domain_name, "80", &result);
+    if (err != 0) {
+        fprintf(stderr, "Can not resolve the domain name.\n");
+        return 1;
+    }
+
+    err = try_connect(result, &sockfd);
+    if (err != 0) {
+        fprintf(stderr, "Can not connect to the host.\n");
+        return 1;
+    }
+    
+    freeaddrinfo(result);
+    
+    char* msg = NULL;
+    
+    err = make_http_request_msg(domain_name, resourсe_url, GET, &msg);
+    
+    if (err != 0) {
+        fprintf(stderr, "Can not generate msg.\n");
+        return 1;
+    }
+
     int len, bytes_sent;
 
     len = strlen(msg);
@@ -245,6 +405,10 @@ int main(int argc, char** argv) {
             }
         }
     }
+
+    if(resourсe_url)
+        free(resourсe_url);
+
     close(sockfd);
-	free_list(list);
+    free_list(list);
 }
