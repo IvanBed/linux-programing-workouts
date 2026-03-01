0a1a2bcb2568aba7af00f8041e158892b07eec36
Ivan Bedaev <ivanbedaev1@gmail.com>
2026-03-01 17:03:41 +0300

    Добавление типов данных в функции

--- src/network-audit-tool/modules/collector.py
+++ src/network-audit-tool/modules/collector.py
@@ -2,26 +2,23 @@ import re
 import platform 
 import subprocess
 
-def get_hostname():
+def get_hostname() -> str:
     pass
 
-def get_os_info():
+def get_os_info() -> str:
     return platform.system() + ' ' + platform.release()
 
 def get_ip_addresses():
     pass
 
-def get_routing_table():
-    pass
-
-def get_next_el(arr_list, word):
+def get_next_el(arr_list: list[str], word: str) -> tuple[bool, str]:
     try:
         idx = arr_list.index(word)
         return False, arr_list[idx + 1] 
     except:
         return True, None
 
-def del_empty(tokens):
+def del_empty(tokens: list[str]) -> list[str]:
     for token in tokens:
         if token == '' or token is None :
             tokens.remove(token)
@@ -29,13 +26,12 @@ def del_empty(tokens):
             token = token.strip()
     return tokens
 
-def get_network_interfaces_info(ip_data):
+def get_network_interfaces_info(ip_data: str) -> tuple[bool, list[dict[str, str]]]:
    
     if ip_data == '':
         return True, None
     
     network_interfaces = []
-    
     rows_pattern = r'^[1-9]+:|\n[1-9]+:'
     
     tokens = del_empty(re.split(rows_pattern, ip_data))
@@ -61,3 +57,51 @@ def get_network_interfaces_info(ip_data):
         network_interfaces.append(network_interface)
     
     return False, network_interfaces
+
+
+def get_routing_table_info(routes_data: str) -> tuple[bool, list[dict[str, str]]]:
+    
+    if routes_data == '':
+        return True, None
+    
+    routing_table_entities = []
+    rows = del_empty(routes_data.splitlines())
+    for row in rows:
+        tokens = row.split(' ')
+        routing_table_entity = {}
+        
+        routing_table_entity['network'] = tokens[0]
+        err, gateway = get_next_el(tokens, 'via')
+        if not err:
+            routing_table_entity['gateway'] = gateway
+        else:
+            routing_table_entity['gateway'] = ' '
+       
+        err, src = get_next_el(tokens, 'src')
+        if not err:
+            routing_table_entity['src'] = src
+        else:
+            routing_table_entity['src'] = ' '
+            
+        err, interface_type = get_next_el(tokens, 'dev')
+        if not err:
+            routing_table_entity['interface_type'] = interface_type
+        else:
+            routing_table_entity['interface_type'] = ' '
+        
+        err, protocol = get_next_el(tokens, 'proto')
+        if not err:
+            routing_table_entity['protocol'] = protocol
+        else:
+            routing_table_entity['protocol'] = ' '
+        
+        err, scope = get_next_el(tokens, 'scope')
+        if not err:
+            routing_table_entity['scope'] = scope
+        else:
+            routing_table_entity['scope'] = ' '
+            
+        routing_table_entities.append(routing_table_entity)    
+        
+    
+    return False, routing_table_entities
