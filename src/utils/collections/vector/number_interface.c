0c186b19811ac248407dbf68b02692285b486aa1
IvanBed <116827491+IvanBed@users.noreply.github.com>
2026-03-23 15:11:30 +0300

    Исправление опечатки

--- src/utils/collections/vector/number_interface.c
+++ src/utils/collections/vector/number_interface.c
@@ -97,7 +97,7 @@ uint8_t set_int(vector *inst, size_t index, int64_t el, uint8_t multithread_flag
     int num = el;
 
     if (!multithread_flag)
-        return set(inst, index, (char *)&num, sizeof(int64_t), (char *)&num);
+        return set(inst, index, sizeof(int64_t), (char *)&num);
     else
         return set_multithread(inst, index, sizeof(int64_t), (char *)&num);
 }
