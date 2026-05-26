8caebd6241b5871fecdc15ea3e3151806cb38a43
IvanBed <116827491+IvanBed@users.noreply.github.com>
2026-05-26 18:34:32 +0300

    Добавление наброска утилиты cp на golang

---
+++ src/file-system/my-cp/my_cp.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const (
+    NOTHING = iota
+    RECURSIVE
+)
+
+func makeOutputPath(destPath string, srcPath string) (string) {
+
+	fi, err := os.Stat(destPath)
+	if err == nil && fi.Mode().IsDir() {
+		if destPath[len(destPath) - 1] != '/' {
+		    destPath = destPath + "/"
+		}
+		destPath = filepath.Dir(destPath) +  "/" + filepath.Base(srcPath)
+	}
+	return destPath
+}
+
+func copyFileInternal() {}
+
+func copyFile(destPath string, srcPath string) {
+	
+	var buff_size int = 4096
+	buff := make([]byte, buff_size)
+	
+	srcFile, err := os.Open(srcPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+    defer srcFile.Close()
+
+	fmt.Println(destPath)
+
+	destFile, err := os.OpenFile(destPath, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+    defer destFile.Close()	
+	
+	for {
+		inputCount, err := srcFile.Read(buff)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if inputCount <= 0 {
+			break
+		}
+
+		outputCount, err := destFile.Write(buff)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if inputCount != outputCount {
+			break
+		}
+	}
+}
+
+func getFlag(mode string) (uint8) {
+
+	var flag uint8
+	
+	for i := 0; i < len(mode); i++ {
+	
+		if mode[i] == 'r' ||  mode[i] == 'R' {
+		    flag |= RECURSIVE
+		}
+	}
+	return flag
+}
+//rewrite with stringBuffer
+func recursiveCopy(destDir string, srcDir string) {	
+	dirEntries, err := os.ReadDir(srcDir)
+	
+	if err != nil {
+		//errors.append()
+	}
+	for _, entry := range dirEntries {
+		entryFullPath := srcDir + "/" + entry.Name()
+		
+		
+		if entry.IsDir() {
+			recursiveCopy(destDir, entryFullPath)
+		} else {	
+			os.MkdirAll(destDir + srcDir, 0666);
+			copyFile(destDir + entryFullPath, entryFullPath)
+		}
+	}
+}
+
+func main() {
+    
+	var srcPath  string
+	var destPath string
+	var flag     uint8
+	argsCount := len(os.Args)
+	
+	if argsCount < 3 {
+		return
+	} else if argsCount == 3 {
+		srcPath  = os.Args[1]
+		destPath = os.Args[2]
+	} else if argsCount == 4 {
+	    flag     = getFlag(os.Args[1])
+		srcPath  = os.Args[2]
+		destPath = os.Args[3]
+	}
+
+	if flag == RECURSIVE {
+		
+		srcPathInfo,  err := os.Stat(srcPath)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		
+		if !srcPathInfo.IsDir() {
+			fmt.Println("Source path is not directory")
+			return		
+		}
+		
+		if destPath[len(destPath) - 1] != '/' {
+		    destPath = destPath + "/"
+		} 
+			
+		recursiveCopy(destPath, srcPath);
+	} else {
+	
+		destPath = makeOutputPath(destPath, srcPath)
+		copyFile(destPath, srcPath)	
+	}	
+}
