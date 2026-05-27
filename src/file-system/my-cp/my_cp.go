f1e20d29225f3b2ed5e335711ca8de9ed8578e31
IvanBed <116827491+IvanBed@users.noreply.github.com>
2026-05-27 15:54:03 +0300

    Добавление возможности передачи нескольких файлов для копирования в утилиту

--- src/file-system/my-cp/my_cp.go
+++ src/file-system/my-cp/my_cp.go
@@ -7,18 +7,18 @@ import (
 )
 
 const (
-    NOTHING = iota
-    RECURSIVE
+	NOTHING = iota
+	RECURSIVE
 )
 
-func makeOutputPath(destPath string, srcPath string) (string) {
-
+func makeOutputPath(destPath string, srcPath string) string {
 	fi, err := os.Stat(destPath)
 	if err == nil && fi.Mode().IsDir() {
-		if destPath[len(destPath) - 1] != '/' {
-		    destPath = destPath + "/"
+		if destPath[len(destPath)-1] != '/' {
+			destPath = destPath + "/"
 		}
-		destPath = filepath.Dir(destPath) +  "/" + filepath.Base(srcPath)
+
+		destPath = filepath.Dir(destPath) + "/" + filepath.Base(srcPath)
 	}
 	return destPath
 }
@@ -26,26 +26,25 @@ func makeOutputPath(destPath string, srcPath string) (string) {
 func copyFileInternal() {}
 
 func copyFile(destPath string, srcPath string) {
-	
+
 	var buff_size int = 4096
 	buff := make([]byte, buff_size)
-	
+
+	fmt.Println("Open src file", srcPath)
 	srcFile, err := os.Open(srcPath)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-    defer srcFile.Close()
-
-	fmt.Println(destPath)
-
+	defer srcFile.Close()
+	fmt.Println("Open dest file ", destPath)
 	destFile, err := os.OpenFile(destPath, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-    defer destFile.Close()	
-	
+	defer destFile.Close()
+
 	for {
 		inputCount, err := srcFile.Read(buff)
 		if err != nil {
@@ -65,79 +64,103 @@ func copyFile(destPath string, srcPath string) {
 			break
 		}
 	}
+	fmt.Println("COPY DONE")
 }
 
-func getFlag(mode string) (uint8) {
+func getFlag(mode string) uint8 {
 
 	var flag uint8
-	
+
 	for i := 0; i < len(mode); i++ {
-	
-		if mode[i] == 'r' ||  mode[i] == 'R' {
-		    flag |= RECURSIVE
+
+		if mode[i] == 'r' || mode[i] == 'R' {
+			flag |= RECURSIVE
 		}
 	}
 	return flag
 }
-//rewrite with stringBuffer
-func recursiveCopy(destDir string, srcDir string) {	
+
+// rewrite with stringBuffer
+func recursiveCopy(destDir string, srcDir string) {
 	dirEntries, err := os.ReadDir(srcDir)
-	
+
 	if err != nil {
 		//errors.append()
 	}
 	for _, entry := range dirEntries {
 		entryFullPath := srcDir + "/" + entry.Name()
-		
-		
+
 		if entry.IsDir() {
 			recursiveCopy(destDir, entryFullPath)
-		} else {	
-			os.MkdirAll(destDir + srcDir, 0666);
-			copyFile(destDir + entryFullPath, entryFullPath)
+		} else {
+			os.MkdirAll(destDir+srcDir, 0755)
+			copyFile(destDir+entryFullPath, entryFullPath)
 		}
 	}
 }
 
 func main() {
-    
-	var srcPath  string
+
+	var srcPath []string
 	var destPath string
-	var flag     uint8
+	var flag uint8
 	argsCount := len(os.Args)
-	
+
 	if argsCount < 3 {
 		return
 	} else if argsCount == 3 {
-		srcPath  = os.Args[1]
+
+		srcPath = append(srcPath, os.Args[1])
 		destPath = os.Args[2]
-	} else if argsCount == 4 {
-	    flag     = getFlag(os.Args[1])
-		srcPath  = os.Args[2]
-		destPath = os.Args[3]
+	} else if argsCount >= 4 {
+		flag = getFlag(os.Args[1])
+
+		for i := 2; i < argsCount-1; i++ {
+			srcPath = append(srcPath, os.Args[i])
+		}
+		destPath = os.Args[argsCount-1]
 	}
 
-	if flag == RECURSIVE {
-		
-		srcPathInfo,  err := os.Stat(srcPath)
+	if len(srcPath) > 1 {
+		destPathInfo, err := os.Stat(destPath)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		
-		if !srcPathInfo.IsDir() {
-			fmt.Println("Source path is not directory")
-			return		
+		if !destPathInfo.IsDir() {
+			fmt.Printf("my_cp: target %s: Not a directory\n", destPath)
+			return
 		}
-		
-		if destPath[len(destPath) - 1] != '/' {
-		    destPath = destPath + "/"
-		} 
-			
-		recursiveCopy(destPath, srcPath);
-	} else {
-	
-		destPath = makeOutputPath(destPath, srcPath)
-		copyFile(destPath, srcPath)	
-	}	
+	}
+
+	currentDestPath := destPath
+	for _, srcFile := range srcPath {
+
+		srcPathInfo, err := os.Stat(srcFile)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if flag == RECURSIVE {
+
+			if !srcPathInfo.IsDir() {
+				currentDestPath = makeOutputPath(destPath, srcFile)
+				fmt.Println(currentDestPath)
+				fmt.Println(srcFile)
+				copyFile(currentDestPath, srcFile)
+			} else {
+				if destPath[len(destPath)-1] != '/' {
+					currentDestPath = destPath + "/"
+				}
+				recursiveCopy(currentDestPath, srcFile)
+			}
+
+		} else {
+			if !srcPathInfo.IsDir() {
+				currentDestPath = makeOutputPath(destPath, srcFile)
+				copyFile(currentDestPath, srcFile)
+			}
+		}
+	}
 }
