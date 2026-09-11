027e4adbfd3b65a6710bb07684913287ab7424da
IvanBed <116827491+IvanBed@users.noreply.github.com>
2026-09-11 17:05:56 +0300

    Добавление возможности писать в продюсер через консоль

--- src/golang/kafka/consumer/consumer.go
+++ src/golang/kafka/consumer/consumer.go
@@ -8,7 +8,7 @@ import (
 	"github.com/IBM/sarama"
 )
 
-func mainLoop(partConsumer sarama.PartitionConsumer, mtx *sync.Mutex, responseChannels map[string]chan *sarama.ConsumerMessage) {
+func mainLoop(partConsumer sarama.PartitionConsumer, consumer sarama.Consumer, mtx *sync.Mutex, responseChannels map[string]chan *sarama.ConsumerMessage) {
 
 	//MIN_COMMIT_COUNT := 1000
 	for {
@@ -19,10 +19,11 @@ func mainLoop(partConsumer sarama.PartitionConsumer, mtx *sync.Mutex, responseCh
 				fmt.Println("Channel closed, exiting goroutine")
 				return
 			}
-
 			responseID := string(msg.Key)
 			msgVal := string(msg.Value)
-			fmt.Println(msgVal)
+			fmt.Printf("сonsumed message values %d offset %d\n", msgVal, msg.Offset)
+			consumer.MarkOffset(msg)
+
 			mtx.Lock()
 			ch, exists := responseChannels[responseID]
 			if exists {
@@ -45,7 +46,7 @@ func main() {
 	}
 	defer consumer.Close()
 
-	partConsumer, err := consumer.ConsumePartition("my_topic", 0, sarama.OffsetNewest)
+	partConsumer, err := consumer.ConsumePartition("my_topic", 0, sarama.OffsetOldest)
 	if err != nil {
 		log.Fatalf("Failed to consume partition: %v", err)
 	}
@@ -53,5 +54,5 @@ func main() {
 
 	fmt.Println("Consumer initialized")
 
-	mainLoop(partConsumer, &mtx, responseChannels)
+	mainLoop(partConsumer, consumer, &mtx, responseChannels)
 }
--- src/golang/kafka/producer/producer.go
+++ src/golang/kafka/producer/producer.go
@@ -1,33 +1,61 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/IBM/sarama"
 )
 
-func asyncConversation(producer sarama.AsyncProducer, signals chan os.Signal) (enqueued int, producerErrors int) {
+type ProducerResults struct {
+	enqueued       int
+	producerErrors int
+}
+
+func write(producer sarama.AsyncProducer, signals chan os.Signal, resultsCh chan ProducerResults) {
+
+	var results ProducerResults
+
+	reader := bufio.NewReader(os.Stdin)
 
 ProducerLoop:
 	for {
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSpace(text)
+		if text == "STOP" {
+			break
+		}
 		select {
-		case producer.Input() <- &sarama.ProducerMessage{Topic: "my_topic", Key: nil, Value: sarama.StringEncoder("testing 123")}:
-			enqueued++
+		case producer.Input() <- &sarama.ProducerMessage{Topic: "my_topic", Key: nil, Value: sarama.StringEncoder(text)}:
+			results.enqueued++
 		case err := <-producer.Errors():
 			log.Println("Failed to produce message", err)
-			producerErrors++
+			results.producerErrors++
 		case <-signals:
 			break ProducerLoop
 		}
 	}
-	return
+	resultsCh <- results
+	close(resultsCh)
 }
 
 func main() {
+	//var wg sync.WaitGroup
 
-	producer, err := sarama.NewAsyncProducer([]string{"localhost:29092"}, nil)
+	args := os.Args
+
+	if len(args) < 3 {
+		fmt.Println("")
+	}
+
+	host := args[1]
+	port := args[2]
+	//localhost:29092
+	producer, err := sarama.NewAsyncProducer([]string{host + ":" + port}, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -40,11 +68,22 @@ func main() {
 
 	// Trap SIGINT to trigger a shutdown.
 	signals := make(chan os.Signal, 1)
+	resultsCh := make(chan ProducerResults, 0)
 	signal.Notify(signals, os.Interrupt)
 
-	var enqueued, producerErrors int
+	var totalEnqueued, totalProducerErrors int
 
-	enqueued, producerErrors = asyncConversation(producer, signals)
+	for i := 1; i <= 1; i++ {
+		//wg.Add(1)
+		go write(producer, signals, resultsCh)
 
-	log.Printf("Enqueued: %d; errors: %d\n", enqueued, producerErrors)
+	}
+
+	for curResults := range resultsCh {
+		totalEnqueued += curResults.enqueued
+		totalProducerErrors += curResults.producerErrors
+	}
+	close(signals)
+	log.Printf("Enqueued: %d; errors: %d\n", totalEnqueued, totalProducerErrors)
+	//wg.Wait()
 }
