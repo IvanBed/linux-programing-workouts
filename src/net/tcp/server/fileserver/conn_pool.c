b50ffac544f03855f4e4a57dbaabdd2b69f32eb2
IvanBed <116827491+IvanBed@users.noreply.github.com>
2026-09-11 17:06:13 +0300

    переименовал store в pool

--- src/net/tcp/server/fileserver/conn_pool.c
+++ src/net/tcp/server/fileserver/conn_pool.c
@@ -4,43 +4,43 @@ ConnectionsPool *create_conn_pool(size_t size)
 {
     size_t mem_size;
     
-    ConnectionsPool *store = (ConnectionsPool*) malloc(sizeof(ConnectionsPool));
-    if (!store)
+    ConnectionsPool *pool = (ConnectionsPool*) malloc(sizeof(ConnectionsPool));
+    if (!pool)
     {
         return 0;
     }
-    memset(store, 0, sizeof(ConnectionsPool));
+    memset(pool, 0, sizeof(ConnectionsPool));
     
     mem_size = size * sizeof(ConnectionDesc);
-    store->connections = (ConnectionDesc*)malloc(mem_size);
-    if (!store->connections)
+    pool->connections = (ConnectionDesc*)malloc(mem_size);
+    if (!pool->connections)
     {
-        free(store);
+        free(pool);
         return 0;
     }  
-    memset(store->connections, 0, mem_size);
+    memset(pool->connections, 0, mem_size);
     
-    if (pthread_mutex_init (&(store->lock), NULL) != 0)
+    if (pthread_mutex_init (&(pool->lock), NULL) != 0)
     {
-        free(store);
-        free(store->connections);
+        free(pool);
+        free(pool->connections);
         return 0;
     }
-    store->free_space_bitmap = INT64_MAX;
-    store->size = size;
-    return store;
+    pool->free_space_bitmap = INT64_MAX;
+    pool->size = size;
+    return pool;
 }
 
-void destruct_conn_pool(ConnectionsPool *store)
+void destruct_conn_pool(ConnectionsPool *pool)
 {
-    if (!store)
+    if (!pool)
     {
 
     }
 
-    free(store->connections);
-    pthread_mutex_destroy(&(store->lock));
-    free(store);
+    free(pool->connections);
+    pthread_mutex_destroy(&(pool->lock));
+    free(pool);
 }
 
 static int64_t book_pos(int64_t *bitmap)
@@ -69,17 +69,22 @@ static void unbook_pos(int64_t *bitmap, int64_t offset)
     *bitmap |= mask;
 }
 
-int64_t store_connection(ConnectionsPool *store, pthread_t thread_id, int connection_sock)
+int64_t store_connection(ConnectionsPool *pool, pthread_t thread_id, int connection_sock)
 {
     int64_t offset;
     
-    if (!store)
+    if (!pool)
     {
 
     }   
+    if (pool->free_space_bitmap == 0)
+    {
+        puts("Pool is full");
+        return NOT_OK;
+    }
 
-    offset = book_pos(&store->free_space_bitmap);
-    if (offset == -1)
+    offset = book_pos(&pool->free_space_bitmap);
+    if (offset == -1 || offset >= pool->size)
     {
         return NOT_OK;
     }
@@ -89,23 +94,23 @@ int64_t store_connection(ConnectionsPool *store, pthread_t thread_id, int connec
     conn.connection_sock = connection_sock;
     conn.offset = offset;
     
-    memcpy(store->connections + offset, &conn, sizeof(ConnectionDesc));
+    memcpy(pool->connections + offset, &conn, sizeof(ConnectionDesc));
   
     return offset;
 }
 
-int64_t release_connection(ConnectionsPool *store, int64_t offset)
+int64_t release_connection(ConnectionsPool *pool, int64_t offset)
 {
-    if (!store)
+    if (!pool)
     {
 
     } 
-    ConnectionDesc *conn = store->connections + offset;
+    ConnectionDesc *conn = pool->connections + offset;
     puts("!release_connection! unbook_pos");
     
     //shutdown(conn->connection_sock, SHUT_RDWR);
     close(conn->connection_sock);
-    unbook_pos(&store->free_space_bitmap, offset);
+    unbook_pos(&pool->free_space_bitmap, offset);
     memset(conn, 0, sizeof(ConnectionDesc));
     return OK;
 }
@@ -123,22 +128,22 @@ int bitmap_contain(int64_t bitmap, int64_t offset)
     return mask == 0;
 }
 
-void print(ConnectionsPool *store)
+void print(ConnectionsPool *pool)
 {
-    if (!store)
+    if (!pool)
     {
 
     }  
 
-    printf("store->size %ld\n", store->size);
-    for (size_t i = 0; i < store->size; i++)
+    printf("pool->size %ld\n", pool->size);
+    for (size_t i = 0; i < pool->size; i++)
     {
-        ConnectionDesc *cur_conn = store->connections + i;
+        ConnectionDesc *cur_conn = pool->connections + i;
         int64_t mask = 1;
         mask <<= i;
         //printf("i %ld\n", i);
-        mask &= store->free_space_bitmap;
-        if (cur_conn && bitmap_contain(store->free_space_bitmap, i))
+        mask &= pool->free_space_bitmap;
+        if (cur_conn && bitmap_contain(pool->free_space_bitmap, i))
         {
            printf("thread %d socket %d offset %ld\n", cur_conn->thread_id, cur_conn->connection_sock, cur_conn->offset);
         }    
